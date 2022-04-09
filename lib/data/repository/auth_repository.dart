03887414fea0a1ba9babc92381f7c40ccac62c42
takeshi.hirosue <mirrored1976@gmail.com>
2022-04-09 18:41:41 +0900

    refactor:

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -9,20 +9,22 @@ class AuthRepository {
   static const String _baseUrl =
       'https://thirosue.github.io/hosting-image2/flutter_study';
 
-  final dio = Dio(BaseOptions(
-    baseUrl: _baseUrl,
-    connectTimeout: 50000,
-    receiveTimeout: 100000,
-    // 5s
-    headers: {
-      HttpHeaders.userAgentHeader: "dio",
-      "api": "1.0.0",
-    },
-    contentType: Headers.jsonContentType,
-    // Transform the response data to a String encoded with UTF8.
-    // The default value is [ResponseType.JSON].
-    responseType: ResponseType.plain,
-  ));
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: _baseUrl,
+      connectTimeout: 50000,
+      receiveTimeout: 100000,
+      // 5s
+      headers: {
+        HttpHeaders.userAgentHeader: "dio",
+        "api": "1.0.0",
+      },
+      contentType: Headers.jsonContentType,
+      // Transform the response data to a String encoded with UTF8.
+      // The default value is [ResponseType.JSON].
+      responseType: ResponseType.plain,
+    ),
+  );
 
   Future<ApiResponse> auth() async {
     Response response;
