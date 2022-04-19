775b738a0a36ce25827c69382121f0a7fcade1ff
takeshi.hirosue <mirrored1976@gmail.com>
2022-04-19 12:33:28 +0900

    feat: toggle swithのデザイン対応

--- lib/ui/card/detail/card_detail_controller.dart
+++ lib/ui/card/detail/card_detail_controller.dart
@@ -5,36 +5,32 @@ import 'package:flutter_with_getx/data/repository/card_repository.dart';
 import 'package:flutter_with_getx/helpers/wait.dart';
 import 'package:get/get.dart';
 
-class CardDetailController extends GetxController with StateMixin<void> {
+class CardDetailController extends GetxController {
   final CardRepository repository;
   final CCard initData;
+
+  final on = false.obs;
 
   CardDetailController({
     required this.repository,
     required this.initData,
   });
 
-  @override
-  void onInit() async {
-    super.onInit();
-    change(null, status: RxStatus.success());
-  }
-
   Future<void> delete() async {
     try {
       EasyLoading.show(status: 'loading...');
       await wait(seconds: 2);
       await repository.delete(initData.id);
-      change(null, status: RxStatus.success());
     } catch (exception, stackTrace) {
       // await Sentry.captureException(exception, stackTrace: stackTrace);
       debugPrintStack(stackTrace: stackTrace);
       debugPrint("error captured...");
-      change(null, status: RxStatus.error('Something is wrong.'));
     } finally {
       EasyLoading.dismiss();
     }
   }
 
-  Future<void> toggle(bool? toggle) async {}
+  Future<void> toggle(bool? toggle) async {
+    on.value = toggle!;
+  }
 }
--- lib/ui/card/detail/card_detail_page.dart
+++ lib/ui/card/detail/card_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_with_getx/component/molecules/card/credit_card_widget.dart';
 import 'package:flutter_with_getx/component/molecules/dialog_actions.dart';
 import 'package:flutter_with_getx/data/const/path.dart';
@@ -9,7 +8,6 @@ import 'package:flutter_with_getx/data/model/card/card_campany.dart';
 import 'package:flutter_with_getx/data/model/card/card_network_type.dart';
 import 'package:flutter_with_getx/data/model/card/validity.dart';
 import 'package:flutter_with_getx/data/repository/card_repository.dart';
-import 'package:flutter_with_getx/helpers/error_handler.dart';
 import 'package:flutter_with_getx/ui/card/detail/card_detail_controller.dart';
 import 'package:get/get.dart';
 
@@ -66,104 +64,112 @@ class CardDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Card Detail'),
       ),
-      body: controller.obx(
-        (state) => Scaffold(
-          body: Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: <Widget>[
-                const SizedBox(
-                  height: 8,
+      body: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 8,
+              ),
+              CreditCard(
+                cardBackground: ImageCardBackground(
+                    const AssetImage('images/background_sample.png')),
+                cardNetworkType: CardNetworkType.mastercard,
+                cardHolderName: 'Gursheesh Singh',
+                cardNumber: '2434 2434 **** ****',
+                company: CardCompany.kotak,
+                validity: Validity(
+                  validThruMonth: 1,
+                  validThruYear: 2023,
                 ),
-                CreditCard(
-                  cardBackground: ImageCardBackground(
-                      const AssetImage('images/background_sample.png')),
-                  cardNetworkType: CardNetworkType.mastercard,
-                  cardHolderName: 'Gursheesh Singh',
-                  cardNumber: '2434 2434 **** ****',
-                  company: CardCompany.kotak,
-                  validity: Validity(
-                    validThruMonth: 1,
-                    validThruYear: 2023,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Obx(
+                () => Container(
+                  padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Table(
+                      children: <TableRow>[
+                        TableRow(
+                          children: <Widget>[
+                            _tableRow(
+                              label: '名前',
+                              value: 'Gursheesh Singh',
+                            )
+                          ],
+                        ),
+                        TableRow(
+                          children: <Widget>[
+                            _tableRow(
+                              label: 'カード番号',
+                              value: '2434 2434 **** ****',
+                            )
+                          ],
+                        ),
+                        TableRow(
+                          children: <Widget>[
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(12, 0, 0, 0),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: _label(
+                                      label: '通知',
+                                    ),
+                                  ),
+                                  Switch(
+                                    value: controller.on.value,
+                                    onChanged: controller.toggle,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(
-                  height: 24,
-                ),
-                Container(
-                    padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Table(
-                        children: <TableRow>[
-                          TableRow(
-                            children: <Widget>[
-                              _tableRow(
-                                label: '名前',
-                                value: 'Gursheesh Singh',
-                              )
-                            ],
-                          ),
-                          TableRow(
-                            children: <Widget>[
-                              _tableRow(
-                                label: 'カード番号',
-                                value: '2434 2434 **** ****',
-                              )
-                            ],
-                          ),
-                          TableRow(
-                            children: <Widget>[
-                              Container(
-                                margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                child: FormBuilderSwitch(
-                                  name: 'notice',
-                                  title: _label(label: '通知'),
-                                  onChanged: controller.toggle,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    )),
-              ],
-            ),
-          ),
-          floatingActionButton: Column(
-            verticalDirection: VerticalDirection.up,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              FloatingActionButton(
-                onPressed: () {
-                  Get.defaultDialog(
-                    title: '確認',
-                    middleText: 'カードを削除します',
-                    radius: 10.0,
-                    actions: DialogActions.create(
-                      context: context,
-                      onConfirm: () async {
-                        await controller.delete();
-
-                        Get.toNamed(Path.card);
-                        Get.snackbar(
-                          'カード削除',
-                          'カードを削除しました',
-                          snackPosition: SnackPosition.BOTTOM,
-                        );
-                      },
-                    ),
-                  );
-                },
-                tooltip: 'カード削除',
-                child: const Icon(Icons.remove),
               ),
             ],
           ),
         ),
-        onError: ErrorHandler.onError,
+        floatingActionButton: Column(
+          verticalDirection: VerticalDirection.up,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                Get.defaultDialog(
+                  title: '確認',
+                  middleText: 'カードを削除します',
+                  radius: 10.0,
+                  actions: DialogActions.create(
+                    context: context,
+                    onConfirm: () async {
+                      await controller.delete();
+
+                      Get.toNamed(Path.card);
+                      Get.snackbar(
+                        'カード削除',
+                        'カードを削除しました',
+                        snackPosition: SnackPosition.BOTTOM,
+                      );
+                    },
+                  ),
+                );
+              },
+              tooltip: 'カード削除',
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
       ),
     );
   }
