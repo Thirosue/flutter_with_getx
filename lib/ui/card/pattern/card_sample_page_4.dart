653dde0b95df47b05f4d66df76c8dff868a6b9ca
takeshi.hirosue <mirrored1976@gmail.com>
2022-06-09 20:52:58 +0900

    wip: add card sample4

--- lib/ui/card/pattern/card_sample_page_4.dart
+++ lib/ui/card/pattern/card_sample_page_4.dart
@@ -1,22 +1,82 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_with_getx/ui/card/pattern/card_sample_controller_4.dart';
-import 'package:get/get.dart';
+import 'package:flutter_with_getx/component/molecules/card/credit_card_widget.dart';
+import 'package:flutter_with_getx/data/model/card/card_background.dart';
 
-class CardSamplePage4 extends StatelessWidget {
-  CardSamplePage4({Key? key}) : super(key: key);
+const duration = Duration(milliseconds: 250);
 
-  final controller = Get.put(CardSampleController4());
+final cardBackgrounds = [
+  SolidColorCardBackground(Colors.black.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.blue.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.yellow.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.red.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.purple.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.black.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.blue.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.yellow.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.red.withOpacity(0.8)),
+  SolidColorCardBackground(Colors.purple.withOpacity(0.8)),
+];
+
+const _alignments = [
+  Alignment(-4.5, 0.5),
+  Alignment(-4, 0.4),
+  Alignment(-3.5, 0.3),
+  Alignment(-3, 0.2),
+  Alignment(0, -0.4),
+  Alignment(4, 0.35),
+  Alignment(4.5, 0.4),
+  Alignment(5, 0.45),
+  Alignment(5.5, 0.5),
+  Alignment(6, 0.55),
+];
+
+double _angle(int index) {
+  if (index < 4) {
+    return -110 * pi / 180;
+  } else if (index == 4) {
+    return 90 * pi / 180;
+  } else {
+    return 110 * pi / 180;
+  }
+}
+
+class CardSamplePage4 extends StatefulWidget {
+  const CardSamplePage4({Key? key}) : super(key: key);
+
+  @override
+  State<CardSamplePage4> createState() => _CardSampleState();
+}
+
+class _CardSampleState extends State<CardSamplePage4> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
+    void _onTap() => setState(() => ++index);
+
+    final cardList = List.generate(10, (i) => i)
+        .map(
+          (i) => AnimatedContainer(
+            duration: duration,
+            alignment: _alignments[(index + i) % 10],
+            child: Transform.rotate(
+              angle: _angle((index + i) % 10),
+              child: CreditCard(
+                cardBackground: cardBackgrounds[i],
+              ),
+            ),
+          ),
+        )
+        .toList();
+
     return Scaffold(
-      body: Obx(
-        () => Stack(
-          children: controller.cards.value,
-        ),
+      body: Stack(
+        children: cardList,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: controller.onTap,
+        onPressed: _onTap,
       ),
     );
   }
